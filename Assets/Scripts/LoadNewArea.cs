08a3c8386a46df8603ddcc17ff60f0429e65c42f
Juhyung Seo <Juhyung Seo>
2016-04-15 19:24:58 +0900

    ..

---
+++ Assets/Scripts/LoadNewArea.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadNewArea : MonoBehaviour
+{
+    public string levelToLoad;
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
